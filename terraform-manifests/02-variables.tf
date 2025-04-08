bbaf2320d3f98ae570d8e285164156d02c2b840b
Edwin Gomes <edwin@invoke-script.com>
2025-04-08 10:03:54 +0100

    Updated

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -7,7 +7,7 @@
 variable "location" {
   type = string
   description = "Azure Region where all these resources will be provisioned"
-  default = "Central US"
+  default = "UK South"
 }
 
 # Azure Resource Group Name
